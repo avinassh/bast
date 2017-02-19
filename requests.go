d719a28d6cac9708944e903935fb273cefbadc95
avi <hi@avi.im>
2017-02-19 12:31:56 +0530

    Refactor requests.go

--- requests.go
+++ requests.go
@@ -11,15 +11,12 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-func main() {
-
+func get_request_for_access_token() *http.Request {
 	app_key := os.Getenv("BAST_APP_KEY")
 	app_secret := os.Getenv("BAST_APP_SECRET")
 	ua_string := os.Getenv("BAST_USER_AGENT_STRING")
 	username := os.Getenv("REDDIT_USERNAME")
 	password := os.Getenv("REDDIT_PASSWORD")
-
-	client := &http.Client{}
 
 	req, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token",
 		strings.NewReader(fmt.Sprintf("grant_type=password&username=%s&password=%s", username, password)),
@@ -30,7 +27,12 @@ func main() {
 	}
 	req.Header.Set("User-Agent", ua_string)
 	req.SetBasicAuth(app_key, app_secret)
+	return req
+}
 
+func get_access_token() string {
+	client := &http.Client{}
+	req := get_request_for_access_token()
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 
@@ -38,9 +40,15 @@ func main() {
 		fmt.Println("Unable to get connect to Reddit")
 		log.Fatal(err)
 	}
-
-	fmt.Println(resp.StatusCode)
 	body, _ := ioutil.ReadAll(resp.Body)
-	jbody := gjson.ParseBytes(body)
-	fmt.Println(jbody)
+	parsed_body := gjson.ParseBytes(body)
+
+	if resp.StatusCode != 200 {
+		log.Fatal("Unable to get Access Token: ", parsed_body)
+	}
+	return parsed_body.Get("access_token").String()
+}
+
+func main() {
+	fmt.Println(get_access_token())
 }
