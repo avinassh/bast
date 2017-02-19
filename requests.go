da7d2bdb601dc75ea5bba035eac740918ab6b61f
avi <hi@avi.im>
2017-02-19 12:14:33 +0530

    Use net/http instead of gorequests

--- requests.go
+++ requests.go
@@ -1,30 +1,46 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
-	"github.com/parnurzeal/gorequest"
-	"github.com/spf13/viper"
+	"github.com/tidwall/gjson"
 )
 
-
 func main() {
-	app_key := viper.GetString("APP_KEY")
-	app_secret := viper.GetString("APP_SECRET")
-	ua_string := viper.GetString("USER_AGENT_STRING")
-	username := viper.GetString("USERNAME")
-	password := viper.GetString("PASSWORD")
-	r := gorequest.New().SetBasicAuth(app_key, app_secret).Set("User-Agent", ua_string)
-	resp, body, errs := r.Post("https://www.reddit.com/api/v1/access_token").Send(
-		map[string]string{
-			"grant_type": "password",
-			"username": username,
-			"password": password,
-		},
-	).End()
-	if errs != nil {
-		fmt.Println(errs)
+
+	app_key := os.Getenv("BAST_APP_KEY")
+	app_secret := os.Getenv("BAST_APP_SECRET")
+	ua_string := os.Getenv("BAST_USER_AGENT_STRING")
+	username := os.Getenv("REDDIT_USERNAME")
+	password := os.Getenv("REDDIT_PASSWORD")
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token",
+		strings.NewReader(fmt.Sprintf("grant_type=password&username=%s&password=%s", username, password)),
+	)
+
+	if err != nil {
+		log.Fatal(err)
 	}
+	req.Header.Set("User-Agent", ua_string)
+	req.SetBasicAuth(app_key, app_secret)
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if err != nil {
+		fmt.Println("Unable to get connect to Reddit")
+		log.Fatal(err)
+	}
+
 	fmt.Println(resp.StatusCode)
-	fmt.Println(body)
+	body, _ := ioutil.ReadAll(resp.Body)
+	jbody := gjson.ParseBytes(body)
+	fmt.Println(jbody)
 }
